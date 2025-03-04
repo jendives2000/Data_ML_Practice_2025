7a62e17d17a8c751b33abdc20705c6d0738fa460
jendives2000 <jendives2000@gmail.com>
2025-03-04 19:49:01 +0200

    changed port setting

--- 1-3-SQL/practice/DuckDB/notebooks/plotly_files/dash_app.py
+++ 1-3-SQL/practice/DuckDB/notebooks/plotly_files/dash_app.py
@@ -20,5 +20,5 @@ app.layout = html.Div(
 
 # Run the app when executed
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))  # Use Render's PORT
+    port = int(os.environ.get("PORT", 1000))  # Use Render's PORT
     app.run_server(debug=False, host="0.0.0.0", port=port)
