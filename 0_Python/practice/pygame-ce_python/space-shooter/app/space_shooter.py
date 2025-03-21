0b11afa37700744bd7bc12229a6cc4403fc911f2
jendives2000 <jendives2000@gmail.com>
2025-03-21 23:06:53 +0200

    refactoring (sprites classes)

--- 0_Python/practice/pygame-ce_python/space-shooter/app/space_shooter.py
+++ 0_Python/practice/pygame-ce_python/space-shooter/app/space_shooter.py
@@ -24,6 +24,17 @@ print(f"UTILS_PATH added: {UTILS_PATH}")
 
 from shared_code_pyg import asset_path
 
+
+# ===== CLASSES WITH SPRITES =====
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(
+            asset_path(os.path.join("space-shooter", "images", "player.png"))
+        ).convert_alpha()
+        self.rect = self.image.get_frect(center=(HALF_WW, HALF_WH * 1.8))
+
+
 # ===== general setup =====
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -38,15 +49,17 @@ surf = pygame.Surface((100, 200))
 surf.fill("white")
 x = 100
 
+player = Player()
+
 # ===== IMPORTING ASSETS =====
 # player graph asset:
-player_surf = pygame.image.load(
-    asset_path(os.path.join("space-shooter", "images", "player.png"))
-).convert_alpha()
-player_rect = player_surf.get_frect(center=(HALF_WW, HALF_WH * 1.8))
-# direction: 1 means left & right is possible, 0 means up & down is impossible
-player_direction = pygame.math.Vector2()
-player_speed = 300
+# player_surf = pygame.image.load(
+#     asset_path(os.path.join("space-shooter", "images", "player.png"))
+# ).convert_alpha()
+# player_rect = player_surf.get_frect(center=(HALF_WW, HALF_WH * 1.8))
+# # direction: 1 means left & right is possible, 0 means up & down is impossible
+# player_direction = pygame.math.Vector2()
+# player_speed = 300
 
 # Star asset
 star_surf = pygame.image.load(
@@ -83,13 +96,15 @@ while running:
         #     player_rect.center = event.pos
 
     # input
-    keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-    # making diagonal moves same speed as horizontal/vertical moves
-    player_direction = player_direction.normalize() if player_direction else player_direction
-    # movement:
-    player_rect.center += player_direction * player_speed * dt
+    # keys = pygame.key.get_pressed()
+    # player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    # player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    # # making diagonal moves same speed as horizontal/vertical moves
+    # player_direction = (
+    #     player_direction.normalize() if player_direction else player_direction
+    # )
+    # # movement:
+    # player_rect.center += player_direction * player_speed * dt
 
     # draw the game, first at the bottom, last on top of all
     display_surface.fill("grey12")
@@ -102,7 +117,8 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
 
     # player_surf:
-    display_surface.blit(player_surf, player_rect)
+    # display_surface.blit(player_surf, player_rect)
+    display_surface.blit(player.image, player.rect)
 
     pygame.display.update()
 
