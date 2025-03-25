6c24f62ae2691270ca87d52c1306d579efaa019d
jendives2000 <jendives2000@gmail.com>
2025-03-25 21:30:18 +0200

    adding walls sprites

--- 0_Python/practice/arcade_python/app/simple_platformer.py
+++ 0_Python/practice/arcade_python/app/simple_platformer.py
@@ -18,17 +18,34 @@ class GameView(arcade.Window):
 
         self.background_color = arcade.csscolor.DODGER_BLUE
 
+        # === PLAYER ===
         self.player_texture = arcade.load_texture(
             ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         )
         self.player_sprite = arcade.Sprite(
             self.player_texture,
-            scale=2,
-            center_x=400,
-            center_y=400,
+            center_x=64,
+            center_y=128,
         )
-        # self.player_sprite.center_x = 64
-        # self.player_sprite.center_y = 128
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
+
+        # === WALLS ===
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(
+                "resources:images/tiles/boxCrate_double.png", scale=0.5
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
 
     def on_resize(self, width, height):
         """This method is automatically called when the window is resized."""
@@ -54,6 +71,8 @@ class GameView(arcade.Window):
 
         # Code to draw other things will go here
         arcade.draw_sprite(self.player_sprite)
+
+        self.player_list.draw()
 
 
 def main():
