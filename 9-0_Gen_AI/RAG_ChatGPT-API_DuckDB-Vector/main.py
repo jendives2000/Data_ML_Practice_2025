1ec2cec5b9414858f75e4dbc44366d2f6c386c0d
jendives2000 <jendives2000@gmail.com>
2025-05-14 11:17:32 +0300

    Renamed 2 columns into movies DF

--- 9-0_Gen_AI/RAG_ChatGPT-API_DuckDB-Vector/main.py
+++ 9-0_Gen_AI/RAG_ChatGPT-API_DuckDB-Vector/main.py
@@ -8,4 +8,14 @@ import pandas as pd
 # Importing the dataset
 pathIMDBcsv = "/home/jendives/MLpro/Data_ML_Practice_2025/9-0_Gen_AI/RAG_ChatGPT-API_DuckDB-Vector/Data-In/IMDB.csv"
 movies_raw = pd.read_csv(pathIMDBcsv)
-movies_raw.head()
+movies_raw.head(10)
+
+# Renaming columns into a new movies df
+movies = movies_raw.rename(
+    columns={
+        "primaryTitle": "movie_title",
+        "Description": "movie_description",
+    }
+)
+
+movies.head(1)
