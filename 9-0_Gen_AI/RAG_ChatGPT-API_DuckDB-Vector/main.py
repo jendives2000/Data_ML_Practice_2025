580a47cc2013590ade54a7f26a75df154b639fa0
jendives2000 <jendives2000@gmail.com>
2025-05-14 11:13:27 +0300

    Dataframe of the dataset

---
+++ 9-0_Gen_AI/RAG_ChatGPT-API_DuckDB-Vector/main.py
@@ -0,0 +1,11 @@
+import os
+
+import pandas as pd
+
+# Checking the OpenAI API key is available
+"OPENAI_API_KEY" in os.environ
+
+# Importing the dataset
+pathIMDBcsv = "/home/jendives/MLpro/Data_ML_Practice_2025/9-0_Gen_AI/RAG_ChatGPT-API_DuckDB-Vector/Data-In/IMDB.csv"
+movies_raw = pd.read_csv(pathIMDBcsv)
+movies_raw.head()
